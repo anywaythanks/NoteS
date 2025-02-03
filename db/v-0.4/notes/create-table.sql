5818d62296e117adc78fa110a87d998ee00a48c4
kabeshev-kd-241113 <anywaythanks@mail.ru>
2025-02-03 15:40:27 +0300

    Упрощение модели

--- db/v-0.4/notes/create-table.sql
+++ db/v-0.4/notes/create-table.sql
@@ -4,7 +4,7 @@ alter table if exists notes
 GO
 
 alter table if exists notes
-    add prev int not null;
+    add prev int null;
 GO
 
 alter table if exists notes
@@ -28,27 +28,5 @@ alter table if exists notes
     add constraint FK_Notes_Note_Type
         foreign key (type)
             references note_types
-
-GO
-
-CREATE FUNCTION defaulter_prev_notes()
-    RETURNS trigger AS
-$$
-BEGIN
-    IF NEW.prev IS NULL THEN
-        NEW.prev := NEW.id;
-    END IF;
-    RETURN NEW;
-END;
-$$
-    LANGUAGE plpgsql;
-
-GO
-
-CREATE TRIGGER trigger_defaulter_prev_notes
-    BEFORE INSERT
-    ON notes
-    FOR EACH ROW
-EXECUTE PROCEDURE defaulter_prev_notes();
 
 GO
